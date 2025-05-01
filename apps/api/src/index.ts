0bf472ac621756bf0d303e998e0300931f79a863
skmayya1 <skandamayya4@gmail.com>
2025-05-01 21:28:17 +0530

    ff

---
+++ apps/api/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/health', (_req, res) => {
+    res.json({ message: 'OK' });
+});
+
+app.listen(PORT, () => {
+    console.log(`API running on port ${PORT}`);
+});
